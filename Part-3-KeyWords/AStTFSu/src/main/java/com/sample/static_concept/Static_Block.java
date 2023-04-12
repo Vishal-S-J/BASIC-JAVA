9ebad6a9018768f14e0dc9eb76a7c02569458772
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-04-11 21:22:31 -0500

    static concept code added

---
+++ Part-3-KeyWords/AStTFSu/src/main/java/com/sample/static_concept/Static_Block.java
@@ -0,0 +1,11 @@
+package com.sample.static_concept;
+
+public class Static_Block {
+    static {
+        System.out.println("Static Block First");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
